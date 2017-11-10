43d0dbc4cb83c69a9d3f3afc7cbc36dcd0f0ecc6
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-10 07:16:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const currentUser='Shireen Ghazala';
 const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting=`Welcome, ${currentUser.string(0,1)}!`;
